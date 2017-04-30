21c7f6df02d4d966ca3dc3d2fed613a25ab3bbe7
bhamman <rhamman@stu.ctcd.edu>
2017-04-29 21:09:29 -0500

    created printInstructions() function

---
+++ camel_game.cpp
@@ -0,0 +1,41 @@
+/*
+Author: Becca Hamman
+Created: 04/28/2017
+Class: ITSY 2421 - Object Oriented Programming
+Instructor: Prof  Welch
+Problem description: Camel Game
+
+Todo - Summary of Modifications:
+04/28/2017 - Analyze and design
+
+Lessons learned:
+
+
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
+using namespace std;
+
+void printInstructions();
+
+int main()
+{
+    printInstructions();
+
+
+    return 0;
+}
+/***************************Functions********************************/
+void printInstructions()
+{
+    cout << "Welcome to the Camel Game!" << endl;
+    cout << "You have stolen a camel to make your";
+    cout <<  " way across the great Mobi desert" << endl;
+    cout << "The natives want the camel back and are chasing you down!";
+    cout << " Survive your desert trek and out run the natives." << endl;
+
+    return;
+}// end void printInstructions()
